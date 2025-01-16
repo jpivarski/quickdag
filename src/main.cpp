83c6f07ac6bb067cc671352333a9df02adbe39c3
jpivarski <jpivarski@gmail.com>
2025-01-16 16:51:54 +0530

    implemented a quickdag

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,108 @@
-#include <pybind11/pybind11.h>
+#include <vector>
+#include <string>
+#include <cstring>
+#include <stdint.h>
 
-int add(int i, int j) { return i + j; }
+template <typename T>
+class DAG {
+public:
+  DAG(): data_(), offsets_({0}) { };
+
+  T new_node(const std::vector<T>& depends_on, const std::string& payload) {
+    size_t payload_bytes = payload.size();
+    size_t payload_size = payload_bytes / sizeof(T) + (payload_bytes % sizeof(T) != 0);
+
+    data_.reserve(data_.size() + 1 + depends_on.size() + 1 + payload_size);
+
+    data_.push_back(depends_on.size());
+    data_.insert(data_.end(), depends_on.begin(), depends_on.end());
+
+    data_.push_back(payload_bytes);
+    size_t payload_start = data_.size();
+    data_.resize(payload_start + payload_size, 0);
+    T* buffer = &data_.data()[payload_start];
+    memcpy(
+      reinterpret_cast<void*>(buffer),
+      reinterpret_cast<const void*>(payload.c_str()),
+      payload_bytes
+    );
+
+    offsets_.push_back(data_.size());
+    return offsets_.size() - 2;
+  }
+
+  std::vector<T> node_depends_on(T node_index) {
+    T data_index = offsets_[node_index];
+    T depends_on_size = data_[data_index];
+
+    return std::vector<T>(
+      data_.begin() + data_index + 1,
+      data_.begin() + data_index + 1 + depends_on_size
+    );
+  }
+
+  std::string node_payload(T node_index) {
+    T data_index = offsets_[node_index];
+    T depends_on_size = data_[data_index];
+    T payload_size = data_[data_index + 1 + depends_on_size];
+    T* buffer = &data_.data()[data_index + 1 + depends_on_size + 1];
+    return std::string(reinterpret_cast<const char*>(buffer), payload_size);
+  }
+
+  size_t nbytes() {
+    return offsets_[offsets_.size() - 1] * sizeof(T);
+  }
+
+  std::vector<std::vector<T>> upstream_map() {
+    std::vector<std::vector<T>> out;
+    for (T i = 0;  i < offsets_.size() - 1;  i++) {
+      out.push_back(node_depends_on(i));
+    }
+    return std::move(out);
+  }
+
+  std::vector<std::vector<T>> downstream_map() {
+    std::vector<std::vector<T>> out;
+    for (T i = 0;  i < offsets_.size() - 1;  i++) {
+      out.push_back(std::vector<T>());
+    }
+    for (T i = 0;  i < offsets_.size() - 1;  i++) {
+      for (T j : node_depends_on(i)) {
+        out[j].push_back(i);
+      }
+    }
+    return std::move(out);
+  }
+
+private:
+  std::vector<T> data_;
+  std::vector<T> offsets_;
+};
+
+///////////////////////////////////////////////////////////////
+
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
 
 namespace py = pybind11;
 
+template <typename T>
+py::class_<DAG<T>>
+make_DAG(const py::handle& m, const char* name) {
+  return py::class_<DAG<T>>(m, name)
+    .def(py::init<>())
+    .def("new_node", &DAG<T>::new_node)
+    .def("node_depends_on", &DAG<T>::node_depends_on)
+    .def("node_payload", [](DAG<T>& self, T node_index) {
+      return py::bytes(self.node_payload(node_index));
+    })
+    .def_property_readonly("nbytes", &DAG<T>::nbytes)
+    .def_property_readonly("upstream_map", &DAG<T>::upstream_map)
+    .def_property_readonly("downstream_map", &DAG<T>::downstream_map)
+  ;
+};
+
 PYBIND11_MODULE(_core, m) {
-  m.doc() = R"pbdoc(
-      Pybind11 example plugin
-      -----------------------
-      .. currentmodule:: python_example
-      .. autosummary::
-         :toctree: _generate
-         add
-         subtract
-  )pbdoc";
-
-  m.def("add", &add, R"pbdoc(
-      Add two numbers
-      Some other explanation about the add function.
-  )pbdoc");
-
-  m.def("subtract", [](int i, int j) { return i - j; }, R"pbdoc(
-      Subtract two numbers
-      Some other explanation about the subtract function.
-  )pbdoc");
+  make_DAG<uint32_t>(m, "DAG32");
+  make_DAG<uint64_t>(m, "DAG64");
 }
--- tests/test_compiled.py
+++ tests/test_compiled.py
@@ -3,9 +3,40 @@ from __future__ import annotations
 import quickdag._core as m
 
 
-def test_add():
-    assert m.add(2, 3) == 5
+def test_construct():
+    dag = m.DAG32()
+
+    node1 = dag.new_node([1, 2, 3], b"hello")
+    node2 = dag.new_node([], b"")
+    node3 = dag.new_node([4, 5], b"there")
+
+    assert node1 == 0
+    assert dag.node_depends_on(node1) == [1, 2, 3]
+    assert dag.node_payload(node1) == b"hello"
+
+    assert node2 == 1
+    assert dag.node_depends_on(node2) == []
+    assert dag.node_payload(node2) == b""
+
+    assert node3 == 2
+    assert dag.node_depends_on(node3) == [4, 5]
+    assert dag.node_payload(node3) == b"there"
+
+    assert dag.nbytes == 60
 
 
-def test_subtract():
-    assert m.subtract(7, 5) == 2
+def test_realistic():
+    dag = m.DAG32()
+
+    source1 = dag.new_node([], b"")
+    source2 = dag.new_node([], b"")
+
+    layer1_node1 = dag.new_node([source1, source2], b"")
+    layer1_node2 = dag.new_node([source1, source2], b"")
+    layer1_node3 = dag.new_node([source1, source2], b"")
+
+    layer2_node1 = dag.new_node([layer1_node1, layer1_node2, layer1_node3], b"")
+    layer2_node2 = dag.new_node([layer1_node1, layer1_node2, layer1_node3], b"")
+
+    assert dag.upstream_map == [[], [], [0, 1], [0, 1], [0, 1], [2, 3, 4], [2, 3, 4]]
+    assert dag.downstream_map == [[2, 3, 4], [2, 3, 4], [5, 6], [5, 6], [5, 6], [], []]
