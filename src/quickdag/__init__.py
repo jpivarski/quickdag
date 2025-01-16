6fc1c8a90ccb9d0a73f958d2a93a72c91c3ebc24
jpivarski <jpivarski@gmail.com>
2025-01-16 14:06:03 +0530

    cookiecutter gh:scientific-python/cookie

---
+++ src/quickdag/__init__.py
@@ -0,0 +1,11 @@
+"""
+Copyright (c) 2025 Jim Pivarski. All rights reserved.
+
+quickdag: A great package.
+"""
+
+from __future__ import annotations
+
+from ._version import version as __version__
+
+__all__ = ["__version__"]
